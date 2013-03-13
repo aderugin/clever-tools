fb52622c62323f95c4de0a43ef5b96ef04271155
Vasiliy Sheredeko <piphon@gmail.com>
2013-03-13 22:43:46 +0600

    Add profile decorator

---
+++ clever/profile.py
@@ -0,0 +1,18 @@
+import time
+import hotshot
+
+
+def profile(prefix):
+    """Декоратор для оценки производительности сайта"""
+    def wrapper(callback):
+        def gen_name():
+            pid = os.getpid()
+            timestr = time.strftime("%Y-%m-%d-%H-%M-%S")
+            return prefix + '.' + str(timestr) + '.' + str(pid) + '.prof'
+        def generate_profile():
+            prof = hotshot.Profile(gen_name())
+            prof.start()
+            callback()
+            prof.stop()
+        return generate_profile
+    return wrapper
