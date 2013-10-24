f4c2dfa3bf6b4410169c614808cc373826715ca5
lantlord <antlord92@gmail.com>
2013-10-24 16:02:59 +0000

    sms gate remaked

--- clever/notifier/backends/sms.py
+++ clever/notifier/backends/sms.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import urllib
 import re
+from django.conf import settings as sts
 
 
 class SmsHostBackend(object):
@@ -9,13 +10,17 @@ class SmsHostBackend(object):
         self.template = template
 
     def send_message(self):
-        print 'sms sending'
         to_phones = self.template.to
         text = self.template.message
 
-        API_ID = u'd4dc9702-f056-9684-2507-fcfbc161b238'
-        SMS_GATE_URL = u'http://sms.ru/sms/send'
+        SMS_GATE_URL = u'http://gate.smsaero.ru/send/'
 
         performed_text = re.sub('\s+', '+', text)
-        prepared_url = unicode(SMS_GATE_URL) + u'?api_id=' + unicode(API_ID) + u'&to=' + unicode(to_phones) + u'&text=' + unicode(performed_text)
+        prepared_url = (unicode(SMS_GATE_URL) +
+            u'?user=' + unicode(sts.SMS_USER) +
+            u'&password=' + unicode(sts.SMS_PASSWORD) +
+            u'&to=' + unicode(to_phones) +
+            u'&text=' + unicode(performed_text) +
+            u'&from=' + unicode(sts.DEFAULT_SMS_FROM)
+        )
         urllib.urlopen(prepared_url.encode('utf-8'))
