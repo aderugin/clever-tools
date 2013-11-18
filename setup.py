b1ba0780b7032e9ffd3acc4210686ba315ee3437
Vasiliy Sheredeko <piphon@gmail.com>
2013-11-18 11:36:52 +0600

    Fix setup.py

--- setup.py
+++ setup.py
@@ -22,17 +22,11 @@ setup(
     include_package_data=True,
     test_suite='clever.tests.runtests.runtests',
     install_requires=[
-        # Concrete version
-        'Django >= 1.4',
-        'django-cache-machine == 0.6',
-
         # Latest version
-        'FeinCMS >= 1.7.1',
         'Pillow == 1.7.8',
         'South >= 0.7.6',
         'Unidecode >= 0.04.12',
         'django-autoslug >= 1.6.1',
-        'django-ckeditor >= 3.6.2.1',
         'django-cleanup >= 0.1.6',
         'django-compressor >= 1.2',
         'django-extensions >= 1.1.0',
@@ -46,10 +40,6 @@ setup(
         "django-fsm >= 1.4.1",
         "templated-emails == 0.6.9",
         "django-eml-email-backend == 0.1",
-        'DjangoSEO == 1.0',
-
-        #'git+git://github.com/alurin/django-whatever.git',
-        #'git+git://github.com/django-mptt/django-mptt.git',
     ],
     scripts=[
         'bin/clever-install-python',
