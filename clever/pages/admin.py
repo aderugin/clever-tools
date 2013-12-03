aa701bfc3049e990b4bb4e3a11a7712c1549d124
Alex Vakhov <alex.vakhov@gmail.com>
2013-12-03 09:18:23 +0600

    add inject_seo_inline from PageAdmin

--- clever/pages/admin.py
+++ clever/pages/admin.py
@@ -7,7 +7,7 @@ from ckeditor.widgets import CKEditorWidget
 from feincms.admin import editor
 # from rollyourown.seo.admin import get_inline
 # from rikitavi.seo import Metadata
-
+from clever.seo.admin import inject_seo_inline
 
 class PageAdminForm(forms.ModelForm):
     template = forms.ChoiceField(choices=Page.page_templates, widget=forms.Select)
@@ -16,7 +16,7 @@ class PageAdminForm(forms.ModelForm):
         super(PageAdminForm, self).__init__(*args, **kwargs)
         self.fields['text'].widget = CKEditorWidget(config_name='default')
 
-
+@inject_seo_inline()
 class PageAdmin(editor.TreeEditor):
     form = PageAdminForm
     list_display = ('title', 'slug', 'path', 'active', 'created_at', 'updated_at', )
