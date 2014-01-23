4fefc9d0448a2a37772029d361c072e7df34e74d
LastDanmer <html.ru@gmail.com>
2014-01-23 15:36:42 +0600

    decorator for model's notifications and message for AjaxFormMixin success

--- clever/core/views.py
+++ clever/core/views.py
@@ -60,6 +60,8 @@ class AjaxProcessMixin(View, AjaxDataMixin):
 
 #-------------------------------------------------------------------------------
 class AjaxFormMixin(object):
+    success_url = '#'
+
     def json_response(self, response):
         return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), mimetype='application/json')
 
@@ -85,6 +87,8 @@ class AjaxFormMixin(object):
         response = {
             'status': True
         }
+        if hasattr(self, 'success_message'):
+            response['message'] = self.success_message
         return response
 
     def form_invalid(self, form):
