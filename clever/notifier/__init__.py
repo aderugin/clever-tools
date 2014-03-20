3e1d74b8fe985ce90fac947367cbba63fda9057a
LastDanmer <html.ru@gmail.com>
2014-03-20 10:27:36 +0600

    prepare_fields_from

--- clever/notifier/__init__.py
+++ clever/notifier/__init__.py
@@ -1,3 +1,11 @@
+def prepare_fields_from(instance):
+    fields = {x.name: None for x in instance._meta.fields}
+    for field in fields:
+        fields[field] = getattr(instance, field)
+    if hasattr(instance, 'get_notifier_context'):
+        fields = instance.get_notifier_context(fields)
+    return fields
+
 def send_message(slug, variables, **kwargs):
     from .models import Notification
     Notification.send(slug, variables, **kwargs)
