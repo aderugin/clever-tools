ca91aaab6338c6fcb611f65db9ef7bcc00ce5a9e
Vasiliy Sheredeko <piphon@gmail.com>
2014-03-31 15:04:35 +0600

    Fix issues with invalidating cache after change attribute

--- clever/catalog/receivers.py
+++ clever/catalog/receivers.py
@@ -12,8 +12,11 @@ from .models import ProductBase as Product
 from .models import ProductAttributeBase as ProductAttribute
 from .models import SectionAttributeBase as SectionAttribute
 from .models import PseudoSectionBase as PseudoSection
+from .models import AttributeBase as Attribute
 
 from .tasks import invalidate_section
+from .tasks import invalidate_brand
+from .tasks import invalidate_attribute
 
 from django.db.models.signals import post_save
 from django.db.models.signals import pre_delete
@@ -36,9 +39,12 @@ def handle_section_invalidate(sender, instance, **kwargs):
         invalidate_section.delay(instance.section.id)
 
     elif issubclass(sender, Brand):
-        sections = instance.descendant_sections.values_list('id', flat=True)
-        for section_id in sections:
-            invalidate_section.delay(section_id)
+        invalidate_brand.delay(instance.id)
 
     elif issubclass(sender, PseudoSection):
         invalidate_section.delay(instance.section_id)
+
+    elif issubclass(sender, Attribute):
+        invalidate_attribute.delay(instance.id)
+
+
