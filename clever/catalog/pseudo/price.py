8367f9a649fbaad5fabaf18e9f7e852b670c1b32
LastDanmer <html.ru@gmail.com>
2014-04-18 18:20:12 +0600

    prices rounding

--- clever/catalog/pseudo/price.py
+++ clever/catalog/pseudo/price.py
@@ -6,6 +6,7 @@ from django.db import models
 from clever.catalog.controls import RangeControl
 from clever.catalog.types import PriceType
 from clever.catalog import settings
+from decimal import Decimal, ROUND_CEILING, ROUND_FLOOR
 
 
 # ------------------------------------------------------------------------------
@@ -24,6 +25,10 @@ class PriceAttribute(PseudoAttribute):
 
     def get_values(self, sections):
         prices = Product.objects.filter(section__in=sections, active=True).aggregate(max_price=models.Max('price'), min_price=models.Min('price'))
+        if isinstance(prices['min_price'], Decimal):
+            prices['min_price'] = prices['min_price'].to_integral_exact(ROUND_FLOOR)
+        if isinstance(prices['max_price'], Decimal):
+            prices['max_price'] = prices['max_price'].to_integral_exact(ROUND_CEILING)
         return (
             (prices['min_price'], prices['min_price']),
             (prices['max_price'], prices['max_price']),
