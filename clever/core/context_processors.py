0311b771d5b1abc152d95ba1b14636f57250805e
Vasiliy Sheredeko <piphon@gmail.com>
2014-07-08 07:49:10 +0600

    Add generate compress scripts and styles command

--- clever/core/context_processors.py
+++ clever/core/context_processors.py
@@ -1,8 +1,13 @@
+from cache_tagging.django_cache_tagging import get_cache
 from django.conf import settings
 
 
 def django_settings(context):
+    # get compress version and increment it
+    cache = get_cache('default')
+    version = cache.get('CLEVER_COMPRESS_VERSION', 1)
     return {
         'DEBUG': settings.DEBUG,
         'TEMPLATE_DEBUG': settings.TEMPLATE_DEBUG,
+        'COMPRESS_VERSION': version,
     }
